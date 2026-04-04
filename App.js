aaa5cf9088caaff9157ea4ffe9e18779e385709d
Balram Prajapati <0105it221045@oriental.ac.in>
2026-04-04 22:21:35 +0530

    Implement user authentication: add registration and login functionality, create user and transaction models, and set up authentication routes

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const connectDB = require('./src/config/db');
 require('dotenv').config();
+const authRoutes = require('./routes/auth.routes');
 
 const app = express();
-// Connect to MongoDB
-connectDB();    
-// Middleware
+
 app.use(express.json());
 //health
 app.get('/api/health', (req, res) => {
   res.json({ status: 'ok', message: 'Zorvyn Assignment API is running' });
 });
+// Routes
+app.use('/auth', authRoutes);
 
 
+
+// Connect to MongoDB
+connectDB();    
 //server listening
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);    
